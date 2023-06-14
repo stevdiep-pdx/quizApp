fcd4493f862f43677be8db601c45925d2ab16a31
stevdiep <stevdiep@pdx.edu>
2023-06-14 17:59:41 -0400

    basic frontend tests created

--- frontend/test/App.test.tsx
+++ frontend/test/App.test.tsx
@@ -6,31 +6,39 @@ import { render, fireEvent, screen } from "@testing-library/react";
 
 // add custom jest matchers from jest-dom
 import "@testing-library/jest-dom";
-import { App } from "../src/App.js";
+import { MemoryRouter } from "react-router-dom";
+import { Home } from "../src/Components/HomePage.js";
+import { Leaderboard } from "../src/Components/Leaderboard.js";
+import { QuizList } from "../src/Components/QuizList.js";
 
-test("Math.sqrt()", () => {
-	expect(Math.sqrt(4)).toBe(2);
-	expect(Math.sqrt(144)).toBe(12);
-	expect(Math.sqrt(2)).toBe(Math.SQRT2);
-});
-
-test("loads and displays greeting", async () => {
-	// Arrange -- This fake-renders our component to a mock browser
-	const testRender = render(<App />);
-	// Act - This simulates a person clicking in the browser
-	fireEvent.click(screen.getByText("count is 0"));
-	// Assert - This tests to make sure the event we just fired did what we expect (increment count)
-	expect(screen.getByText("count is 1")).toBeVisible();
-});
-
-describe("Renders React components correctly", async () => {
-	it("Should render the page correctly", async () => {
-		const testRender = render(<App />);
-		// Setup
-		const h1 = await screen.queryByText("Vite + React");
-
-		// Expectations
-		expect(h1).not.toBeNull();
-		expect(h1).toBeVisible();
+describe("Renders React components correctly", () => {
+	test("Math.sqrt()", () => {
+		expect(Math.sqrt(4)).toBe(2);
+		expect(Math.sqrt(144)).toBe(12);
+		expect(Math.sqrt(2)).toBe(Math.SQRT2);
+	});
+	
+	it("Should render homepage correctly", () => {
+		render(<Home />);
+		const elem = screen.queryByText("Quizr");
+		expect(elem)
+			.not
+			.toBeNull();
+		expect(elem)
+			.toBeVisible();
+	});
+	
+	test("Loads Leaderboard", () => {
+		render(<MemoryRouter><Leaderboard /></MemoryRouter>);
+		const elem = screen.queryByText("Leaderboard");
+		expect(elem)
+			.toBeVisible();
+	});
+	
+	test("Loads Quizzes", () => {
+		render(<MemoryRouter><QuizList /></MemoryRouter>);
+		const elem = screen.queryByText("Select a Quiz");
+		expect(elem)
+			.toBeVisible();
 	});
 });
